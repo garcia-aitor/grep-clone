de92bf7769b0f06c51ea9808027e068344d0961a
Aitor <aitorchain@gmail.com>
2026-04-20 11:52:30 +0200

    finish

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module mgrep
 
 go 1.26.2
+
+require github.com/alexflint/go-arg v1.6.1
+
+require github.com/alexflint/go-scalar v1.2.0 // indirect
--- worker/worker.go
+++ worker/worker.go
@@ -1,5 +1,12 @@
 package worker
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type Result struct {
 	Line string
 	LineNum int
@@ -16,5 +23,27 @@ func NewResult(line string, lineNum int, path string) Result {
 
 func FindInFile(path string, find string) * Results {
 	file, err := os.Open(path)
-	
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return nil
+	}
+
+	results := Results{make([]Result, 0)}
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 1
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), find) {
+			r := NewResult(scanner.Text(), lineNum, path)
+			results.Inner = append(results.Inner, r)
+		}
+		lineNum += 1
+	}
+
+	if len(results.Inner) == 0 {
+		return nil
+	} else {
+		return &results
+	}
 }
